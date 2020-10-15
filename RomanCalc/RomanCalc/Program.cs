dc3b4136912399cda2aaed9d6d39381a15aad0a8
hertote <71886137+hertote@users.noreply.github.com>
2020-10-15 17:46:35 +0200

    Second Commit RomanCalc
    the second summit of a RomanCalcApp

--- RomanCalc/RomanCalc/Program.cs
+++ RomanCalc/RomanCalc/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+//Este es el controlador principal
 namespace RomanCalc
 {
     class Program
     {
+        public static void LaunchMainMenu()
+        {
+            while (true) //es un bucle continuo para que la app no cierre
+            {
+                System.Console.Clear();
+                UserInterface.PrintMainMenu();
+                string option = System.Console.ReadLine();
+                System.Console.WriteLine("La opción introducida es " + option);
+                
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LaunchMainMenu();
+
+
+
         }
     }
 }
---
+++ RomanCalc/RomanCalc/UserInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanCalc
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+            System.Console.WriteLine("Calculadora Román");
+            System.Console.WriteLine("-----------------");
+            System.Console.WriteLine("1) Sumar una serie de numeros.");
+            System.Console.WriteLine("2) Restar una serie de numeros.");
+            System.Console.WriteLine("3) Multiplicar una serie de numeros.");
+            System.Console.WriteLine("4) Dividir una serie de numeros.");
+            System.Console.WriteLine("5) Realizar ecuación de primer grado.");
+            System.Console.WriteLine("6) Realizar ecuación de segundo grado.");
+            System.Console.WriteLine("7) Realizar Raíz cuadrada.");
+            System.Console.WriteLine("8) Realizar secuencia fibonacci.");
+            System.Console.WriteLine("9) Realizar Sumario.");
+            System.Console.WriteLine("0) Salir.");
+           
+
+
+
+
+
+
+
+        }
+    }
+}
